fd28dc076105aabb4d50e43fb2412cba0cce9c1d
GabrAlvSM <102328994+GabrAlvSM@users.noreply.github.com>
2024-05-23 10:59:39 -0400

    renamed:    Aula23.05/listas.py -> Aula23.05/listasTeste.py 	new file:   Aula23.05/terefaListas.py 	modified:   AulaLista2 (modified content) 	modified:   ProjetoEstoque (modified content)

---
+++ Aula23.05/terefaListas.py
@@ -0,0 +1,15 @@
+list_num = [10,20,30,40.5,50,60.5,70,80.8,90,100]
+
+list_num.pop(-1)
+print(list_num, "\n")
+
+list_num.append(111)
+list_num.append(122)
+list_num.append(133)
+print(list_num, "\n")
+
+list_num.insert(-3, 66)
+print(list_num, "\n")
+
+list_num.pop(-3)
+print(list_num, "\n")
