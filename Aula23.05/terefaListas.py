522349da446b023d09b9f360168eab62ad911cbc
GabrAlvSM <102328994+GabrAlvSM@users.noreply.github.com>
2024-05-23 11:24:33 -0400

    modified:   Aula23.05/terefaListas.py

--- Aula23.05/terefaListas.py
+++ Aula23.05/terefaListas.py
@@ -1,4 +1,4 @@
-list_num = [10,20,30,40.5,50,60.5,70,80.8,90,100]
+list_num = [30,20,10,40.5,60.5,50,70,80.8,90,100]
 
 list_num.pop(-1)
 print(list_num, "\n")
@@ -13,3 +13,20 @@ print(list_num, "\n")
 
 list_num.pop(-3)
 print(list_num, "\n")
+
+print("Comprimento:", len(list_num), "\n")
+
+soma = sum(list_num)
+print(soma, "\n")
+
+media = (sum(list_num) / len(list_num))
+print(media, "\n")
+
+list_num.sort()
+print(list_num, "\n")
+
+list_num.sort(reverse=True)
+print(list_num, "\n")
+
+list_num.sort()
+print(list_num[0], list_num[-1])
