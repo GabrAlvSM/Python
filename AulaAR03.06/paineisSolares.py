af1719a03da51278f9c12ecd6a6c4c583b9b919c
GabrAlvSM <gabriel.alvin7@gmail.com>
2024-06-05 17:23:47 -0400

    modified:   AulaAR03.06/paineisSolares.py

--- AulaAR03.06/paineisSolares.py
+++ AulaAR03.06/paineisSolares.py
@@ -1,5 +1,4 @@
-# Calcular quantidade de painéis solares para uma residência. Para implementar as quatro demandas acima, 
-# você deverá listar os requisitos necessários para o desenvolvimento, elaborar o diagrama de caso de uso e por fim o algoritmo utilizando o Python.
+# Calcular quantidade de painéis solares para uma residência. 
 
 # Fórmula — [Potência do painel X Irradiação (1 – Percentual de perda)];
 
@@ -10,12 +9,14 @@
 
 ### Energia = potência do módulo solar x tempo x (rendimento)
 
-placa = 550
+placa = 265
 perda = 0.2
 irradiac = 5
 
-placagera = placa * irradiac * (1-perda)
+placagera = ((placa * irradiac * (1 - perda))/1000) * 30
 
 consumo = float(input("Informe quantos kWh são gastos por mês na casa: "))
 
-print(placagera)
+qntPlacas = consumo / placagera
+
+print(f"O consumo da casa equivale a aproximadamente {round(qntPlacas)} placas de {placa}Wp que geram individualmente {placagera}kWh de energia mensal.")
