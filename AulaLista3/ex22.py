7a6a6832934620cf80c522fcac7803a03eb658e6
GabrAlvSM <102328994+GabrAlvSM@users.noreply.github.com>
2024-06-19 11:00:46 -0400

    Ex 22 done

--- AulaLista3/ex22.py
+++ AulaLista3/ex22.py
@@ -4,3 +4,35 @@
 # fornecido ao programa, o qual terminará quando for introduzido um valor que não seja válido
 # como nota de aprovação.
 
+## MELHORADO - Sistema de média funcional com calculo automático de média com o usuário apenas inserindo as notas
+
+notas_lista = []
+
+while (True):
+    nota = input("Digite uma nota: ")
+
+    if nota == '':
+        print(f"\n{len(notas_lista)} notas adicionadas.\n")
+        break
+
+    else:
+        nota = int(nota)
+
+        if 0 <= nota <= 10:
+            notas_lista.append(nota)
+            print(f"Notas já contabilizadas: \n{notas_lista}")
+        else:
+            print("\n--Notas válidas apenas de 0 a 10!--\n")
+
+# print(notas_lista, "lista de notas adicionadas")
+
+soma = 0
+i=0
+while (i != len(notas_lista)):
+    x = notas_lista[i]
+    soma += x
+    i+=1
+
+media = soma / len(notas_lista)
+
+print(f"A média do aluno é: {media}")
