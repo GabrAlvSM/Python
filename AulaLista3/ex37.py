8c8c524f92d2accf4223cf230bbcd9e277929398
GabrAlvSM <102328994+GabrAlvSM@users.noreply.github.com>
2024-07-04 11:43:27 -0400

    Prime exercises update
    Trying to get the prime numbers via square root of themselves

--- AulaLista3/ex37.py
+++ AulaLista3/ex37.py
@@ -1,9 +1,45 @@
 # Escreva um programa que receba um número inteiro maior do que 1, e verifique se o número
 # fornecido é primo ou não.
 
-num = int(input("Digite um número: "))
 
-if (num % 2 != 0 and num % 3 != 0 or num == 2 or num == 3):
-    print("primo!")
-else:
-    print("não primo!")
+# if (num % 2 != 0 and num % 3 != 0 or num == 2 or num == 3):
+#     print("primo!")
+# else:
+#     print("não primo!")
+
+#####      Divide the given number by all the prime numbers below its square root value
+
+# while (True):
+
+#     if (num == 0):
+#         print("Fim!")
+#         break
+
+
+num = float(input("\nDigite um número: "))
+
+raizNum = num**(1/2)
+
+
+while (num > 0):
+            
+    if (num % 2 != 0 and num > 3):
+        i=3
+        while(i < raizNum):
+            if (num % i == 0):
+                print("não primo!")
+                break
+            i+=2
+        if (i >= raizNum):
+            print("primo")
+        break
+    
+    elif (num % 2 == 0 and num != 2):
+        print("não primo!")
+        break
+
+    else:
+        print("primo!")
+        break
+        
+print("Fim!\n")
--- AulaLista3/ex39.py
+++ AulaLista3/ex39.py
@@ -4,16 +4,55 @@ inicioInter = int(input("Digite o valor do início do intervalo: "))
 fimInter = int(input("Digite o valor do fim do intervalo: "))
 
 soma = 0
-i=inicioInter
-while (True):
-    if ((i % 2 != 0) and (i % 3 != 0) and (i % 5 != 0) and (i % 7 != 0) or (i == 2) or (i == 3) or (i==5) or (i==7)):
-        soma+=i
-        print("primo",i)
+interv = fimInter - inicioInter
 
-    elif (i==0):
-        print("Fim!", soma)
-        break
-    i-=1
+i = inicioInter
+root_i = i**(1/2)
 
 
-    ############ IMCOMPLETO
+if (inicioInter >= 0 and fimInter > inicioInter):
+    # while (True):
+    #     if ((i % 2 != 0) and (i % 3 != 0) and (i % 5 != 0) and (i % 7 != 0) or (i == 2) or (i == 3) or (i==5) or (i==7)):
+    #         soma+=1
+    #         print("primo",i)
+
+    #     elif (interv==0):
+    #         print("Fim!", soma)
+    #         break
+        
+    #     interv-=1
+    #     i+=1
+    ########
+
+    while (interv > 0):
+                
+        if (i % 2 != 0 and i > 3):
+            x=3
+            while(x < root_i):
+                if (i % x == 0):
+                    print("não primo!", i)
+                    break
+                x+=1
+                if (i >= root_i):
+                    print("primo", i)
+                    soma+=1
+                    break
+                
+        
+        elif (i % 2 == 0 and i != 2):
+            print("não primo!", i)
+            
+
+        else:
+            print("primo!", i)
+            soma+=1
+            
+    
+        interv-=1
+        i+=1
+
+
+else:
+    print("O início do intervalo deve ser positivo e o valor do final do intervalo deve ser maior que inicial!")
+
+print("yo yo yo yooo",soma)
