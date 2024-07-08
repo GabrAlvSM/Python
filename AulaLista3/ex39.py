bae2500f6c7a709a4ee97ccf6ba78633352613e3
GabrAlvSM <gabriel.alvin7@gmail.com>
2024-07-08 02:33:44 -0400

    .

--- AulaLista3/ex39.py
+++ AulaLista3/ex39.py
@@ -3,51 +3,36 @@
 inicioInter = int(input("Digite o valor do início do intervalo: "))
 fimInter = int(input("Digite o valor do fim do intervalo: "))
 
-soma = 0
 interv = fimInter - inicioInter
 
 i = inicioInter
 root_i = i**(1/2)
 
-
-if (inicioInter >= 0 and fimInter > inicioInter):
-    # while (True):
-    #     if ((i % 2 != 0) and (i % 3 != 0) and (i % 5 != 0) and (i % 7 != 0) or (i == 2) or (i == 3) or (i==5) or (i==7)):
-    #         soma+=1
-    #         print("primo",i)
-
-    #     elif (interv==0):
-    #         print("Fim!", soma)
-    #         break
-        
-    #     interv-=1
-    #     i+=1
-    ########
+contador = 0
+if (inicioInter >= 0 and interv >= 0):
 
     while (interv > 0):
                 
-        if (i % 2 != 0 and i > 3):
-            x=3
+        if (i % 2 != 0 and i > 1):
+            x=1
             while(x < root_i):
-                if (i % x == 0):
-                    print("não primo!", i)
+                if (i % x != 0):
+                    print("primo!", i)
+                    contador+=1
                     break
-                x+=1
-                if (i >= root_i):
-                    print("primo", i)
-                    soma+=1
-                    break
+                x+=2
                 
-        
-        elif (i % 2 == 0 and i != 2):
-            print("não primo!", i)
-            
-
-        else:
+            # if (x >= root_i):
+                # print("não primo", i)
+                # interv-=1
+              
+        elif (i % 2 != 0 or i == 2 or i == 3):
             print("primo!", i)
-            soma+=1
+            contador+=1
+        
+        # else:
+        #     print("primo!", i)
             
-    
         interv-=1
         i+=1
 
@@ -55,4 +40,19 @@ if (inicioInter >= 0 and fimInter > inicioInter):
 else:
     print("O início do intervalo deve ser positivo e o valor do final do intervalo deve ser maior que inicial!")
 
-print("yo yo yo yooo",soma)
+print(f"Quantidade de primos no intervalo de {inicioInter} a {fimInter}: {contador}")
+
+
+
+    # while (True):
+    #     if ((i % 2 != 0) and (i % 3 != 0) and (i % 5 != 0) and (i % 7 != 0) or (i == 2) or (i == 3) or (i==5) or (i==7)):
+    #         contador+=1
+    #         print("primo",i)
+
+    #     elif (interv==0):
+    #         print("Fim!", contador)
+    #         break
+        
+    #     interv-=1
+    #     i+=1
+    ########
