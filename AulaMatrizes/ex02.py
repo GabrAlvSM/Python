432d5e40fa5ffc70cfac659c7d88a2347535b1cd
GabrAlvSM <102328994+GabrAlvSM@users.noreply.github.com>
2024-07-11 11:44:14 -0400

    .

---
+++ AulaMatrizes/ex02.py
@@ -0,0 +1,11 @@
+# Utilizando a estrutura de repetição While crie um programa que inicialize uma matriz 4 x 4, o usuário deve digitar os dados de entrada de cada elemento da matriz. Durante a execução o programa deve informar qual linha e coluna o usuário está preenchendo. 
+# Ao final imprima a matriz preenchida pelo usuário. 
+#
+# Entrada: dados
+#
+# Saída: 
+# ['12','11','10','32']
+# ['40','50','44','10']
+# ['99','12','15','16']
+# ['18','19','20','22']
+
