a3c4a9a09863d89e875af7ae1be7e77d19b4b7cc
GabrAlvSM <102328994+GabrAlvSM@users.noreply.github.com>
2024-07-16 11:46:04 -0400

    new file:   AulaDicionarios/exTeste.py

---
+++ AulaDicionarios/exTeste.py
@@ -0,0 +1,16 @@
+dictionary = {
+  "brand": "Ford",
+  "electric": False,
+  "year": [1964, 2024],
+  "colors": ["red", "white", "blue"]
+}
+print(type(dictionary))
+
+# print(len(dictionary["colors"]))
+
+print(dictionary["year"][1])
+
+
+# dicionario_teste = dict(name = "John", age = 36, country = "Norway")
+print(dictionary.keys())
+print(dictionary.items())
